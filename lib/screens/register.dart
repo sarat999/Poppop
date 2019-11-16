2dde91d8172f30317bdda456850bab63462eff92
pop <saratno1@gmail.com>
2019-11-16 15:34:26 +0700

    Design UI register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,13 +6,94 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Filed
+
+  // Method
+  Widget nameText() {
+    Color color = Colors.red.shade900;
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.face,
+          color: color,
+          size: 48.0,
+        ),
+        labelText: 'Display Name :',
+        labelStyle: TextStyle(color: color),
+        helperText: 'Type Your Name',
+        helperStyle: TextStyle(color: color),
+        hintText: 'English Only',
+      ),
+    );
+  }
+
+  Widget emailText() {
+    Color color = Colors.purple.shade900;
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.email,
+          color: color,
+          size: 48.0,
+        ),
+        labelText: 'Email :',
+        labelStyle: TextStyle(color: color),
+        helperText: 'Your Email',
+        helperStyle: TextStyle(color: color),
+        hintText: 'you@email.com',
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    Color color = Colors.green.shade900;
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          color: color,
+          size: 48.0,
+        ),
+        labelText: 'Password :',
+        labelStyle: TextStyle(color: color),
+        helperText: 'Type Your Password',
+        helperStyle: TextStyle(color: color),
+        hintText: 'More 6 Charactor',
+      ),
+    );
+  }
+
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red[900],
         title: Text('Register'),
+        actions: <Widget>[registerButton()],
       ),
-      body: Text('body'),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+          colors: [Colors.white, Colors.blue.shade700],
+          radius: 1.5,center: Alignment.topCenter,
+        )),
+        child: ListView(
+          padding: EdgeInsets.all(30.0),
+          children: <Widget>[
+            nameText(),
+            emailText(),
+            passwordText(),
+          ],
+        ),
+      ),
     );
   }
 }
